19ea4c2271a05da84fd09a28cc9dc3326a7b9abf
Noemiegrau <noemiegrau@outlook.fr>
2021-04-08 15:51:55 -0700

    added validate function to certain answers

--- app.js
+++ app.js
@@ -15,12 +15,28 @@ return inquirer.prompt([
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name?'
+            message: 'What is your name?',
+            validate: nameInput => {
+              if (nameInput) {
+                return true;
+              } else {
+                console.log('Please enter your name!');
+                return false;
+              }
+            }
         },
         {
             type: 'input',
             name: 'github',
-            message: 'Enter your GitHub Username'
+            message: 'Enter your GitHub Username',
+            validate: githubInput => {
+              if (githubInput) {
+                return true;
+              } else {
+                console.log('Please enter your github name!');
+                return false;
+              }
+            }
         },
         {
             type: 'input',
@@ -49,12 +65,28 @@ promptUser()
       {
         type: 'input',
         name: 'name',
-        message: 'What is the name of your project?'
+        message: 'What is the name of your project?',
+        validate: projectNameInput => {
+          if (projectNameInput) {
+            return true;
+          } else {
+            console.log('Please enter your project name!');
+            return false;
+          }
+        }
       },
       {
         type: 'input',
         name: 'description',
-        message: 'Provide a description of the project (Required)'
+        message: 'Provide a description of the project (Required)',
+        validate: projectDescInput => {
+          if (projectDescInput) {
+            return true;
+          } else {
+            console.log('Please enter your project description!');
+            return false;
+          }
+        }
       },
       {
         type: 'checkbox',
@@ -65,7 +97,15 @@ promptUser()
       {
         type: 'input',
         name: 'link',
-        message: 'Enter the GitHub link to your project. (Required)'
+        message: 'Enter the GitHub link to your project. (Required)',
+        validate: linkInput => {
+          if (linkInput) {
+            return true;
+          } else {
+            console.log('Please enter your project links!');
+            return false;
+          }
+        }
       },
       {
         type: 'confirm',
