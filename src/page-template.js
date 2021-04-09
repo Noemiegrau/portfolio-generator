d1b7f359a617147af28a7fd4ce4244e45b36a799
Noemiegrau <noemiegrau@outlook.fr>
2021-04-09 16:11:44 -0700

    added about section function

--- src/page-template.js
+++ src/page-template.js
@@ -1,5 +1,19 @@
 module.exports = templateData => {
 
+// create the about section
+const generateAbout = aboutText => {
+  if (!aboutText) {
+    return '';
+  }
+
+  return `
+    <section class="my-3" id="about">
+      <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
+      <p>${aboutText}</p>
+    </section>
+  `;
+};
+
   // destructure projects and about data from templateData based on their property key names
   // this will create three variables based on data in templateData
   // destructure page data by section
@@ -31,7 +45,7 @@ module.exports = templateData => {
       </div>
     </header>
     <main class="container my-5">
-
+      ${generateAbout(about)}
     </main>
     <footer class="container text-center py-3">
       <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${header.name}</h3>
