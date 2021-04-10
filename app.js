1c945780bcdb4b9798bd1755dae775201a3903cd
Noemiegrau <noemiegrau@outlook.fr>
2021-04-09 18:16:25 -0700

    export the functions and cleaned code up

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
 
